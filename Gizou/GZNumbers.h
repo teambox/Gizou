68dcb0583b0df2f32fc794294765b8673eebe39b
Maria Bernis <mariabernis.clavijo@gmail.com>
2015-03-02 00:59:01 +0100

    Updated GZNumbers API with new features and simplified methods names, added some documentation and tests

--- Gizou/GZNumbers.h
+++ Gizou/GZNumbers.h
@@ -11,10 +11,138 @@
 + (NSNumber *)randomBOOL;
 + (NSNumber *)randomInteger;
 + (NSNumber *)randomNonZeroInteger;
-+ (NSNumber *)randomIndex:(id)enumerable;
 + (NSNumber *)randomIntegerLessThan:(int32_t)lessThan;
 + (NSNumber *)randomIntegerStartingAt:(int32_t)startingAt;
 + (NSNumber *)randomIntegerBetween:(int32_t)min and:(int32_t)max;
++ (NSNumber *)randomIndex:(id)enumerable;
 + (NSNumber *)randomFloatBetween:(float)min and:(float)max;
 
 @end
+
+
+@interface GZNumbers (Booleans)
+
+/**
+ *
+ *  @return YES, NO
+ */
++ (NSNumber *)yesOrNo;
+
+/**
+ *
+ *  @param m Predominant bool value
+ *
+ *  @return YES, NO with `mostly` value more than 80% of the times.
+ */
++ (NSNumber *)yesOrNoMostly:(BOOL)m;
+
+@end
+
+
+@interface GZNumbers (NaturalNumbers)
+
+/**
+ *  Any natural number including 0 (ie. unsigned integers).
+ *
+ *  @return 0, 1, 2,...,INT32_MAX-1
+ */
++ (NSNumber *)integerN;
+
+/**
+ *  Any natural number excluding 0.
+ *
+ *  @return 1, 2,..., INT32_MAX
+ */
++ (NSNumber *)integerNNonZero;
+
+/**
+ *  Any natural number from 0 to MAX.
+ *  This method just invokes `integerNBetween:0 and:MAX`.
+ *
+ *  @param max Maximum desired value that may be generated.
+ *
+ *  @return 0, 1,..., MAX
+ *
+ *  @see `integerNBetween:and:`
+ */
++ (NSNumber *)integerNLessOrEqual:(u_int32_t)max;
+
+/**
+ *  Any natural number from MIN to INT32_MAX.
+ *  This method just invokes `integerNBetween:MIN and:INT32_MAX`.
+ *
+ *  @param min Minimum desired value that may be generated.
+ *
+ *  @return MIN, MIN+1, MIN+2,..., INT32_MAX
+ *
+ *  @see `integerNBetween:and:`
+ */
++ (NSNumber *)integerNBiggerOrEqual:(u_int32_t)min;
+
+/**
+ *  Any natural number from MIN to MAX.
+ *
+ *  @param min Minimum desired value that may be generated.
+ *  @param max Maximum desired value that may be generated.
+ *
+ *  @return MIN, MIN+1, MIN+2,..., MAX
+ *
+ *  @discussion MIN and MAX must be unsigned integers less than INT32_MAX. If not, INT32_MAX will be used for that parameter.
+ */
++ (NSNumber *)integerNBetween:(u_int32_t)min and:(u_int32_t)max; //min,..max
+
+
+///--------------------------------
+//  @name Asymmetric natural numbers
+///--------------------------------
+
+/**
+ *  Any natural number from MIN to MAX with predominant value MANY and/or scarce value FEW if provided. Both MANY and FEW are optional parameters.
+ *
+ *  @param min Minimum desired value that may be generated.
+ *  @param max Maximum desired value that may be generated.
+ *  @param m   Number with the desired integer to be predominant.
+ *  @param f   Number with the desired integer to be scarce.
+ *
+ *  @return MIN, MIN+1, MIN+2,..., MAX with `MANY` value ca 70% of the times and FEW less than 10% of the times aprox, depending on the range.
+ *
+ *  @discussion Same integer limits apply as in `integerNBetween:and`
+ *
+ *  @see `integerNBetween:and:`
+ */
++ (NSNumber *)integerNBetween:(u_int32_t)min and:(u_int32_t)max many:(NSNumber *)m few:(NSNumber *)f;
+
+@end
+
+
+@interface GZNumbers (ZNumbers)
+
++ (NSNumber *)integerZ; // all integers: ..,-2, -1, 0, 1, 2,...
++ (NSNumber *)integerZNonZero; // .., -2, -1, 1, 2,...
++ (NSNumber *)integerZEndingAt:(int32_t)max; // .., -2, -1, 0, 1,...,n
++ (NSNumber *)integerZStartingAt:(int32_t)min; // n, n+1,...
++ (NSNumber *)integerZBetween:(int32_t)min and:(int32_t)max; // min,..,max
+
++ (NSNumber *)negativeInteger; // all negative integers: ..,-2, -1, 0
++ (NSNumber *)negativeIntegerNonZero; // .., -2, -1
+
++ (NSNumber *)integerZBetween:(int32_t)min and:(int32_t)max many:(NSNumber *)m few:(NSNumber *)f;
+
+@end
+
+
+@interface GZNumbers (DecimalNumbers)
+
++ (NSNumber *)randomFloatBetween:(float)min and:(float)max;
+
+@end
+
+
+@interface GZNumbers (Indexes)
+
++ (NSNumber *)indexFrom:(id)enumerableObj;
++ (NSNumber *)indexFrom:(id)enumerableObj many:(NSNumber *)m few:(NSNumber *)f;
+
+@end
+
+
--- Gizou/GZNumbers.m
+++ Gizou/GZNumbers.m
@@ -8,6 +8,9 @@
 
 #import "GZNumbers.h"
 
+#define kINTMAX INT32_MAX
+
+
 @implementation GZNumbers
 
 + (NSNumber *)randomBOOL
@@ -17,12 +20,12 @@
 
 + (NSNumber *)randomInteger
 {
-    return @(arc4random_uniform(INT32_MAX));
+    return @(arc4random_uniform(kINTMAX));
 }
 
 + (NSNumber *)randomNonZeroInteger
 {
-    return @(arc4random_uniform(INT32_MAX) + 1);
+    return @(arc4random_uniform(kINTMAX) + 1);
 }
 
 + (NSNumber *)randomIndex:(id)enumerable
@@ -49,7 +52,7 @@
 
 + (NSNumber *)randomIntegerStartingAt:(int32_t)startingAt
 {
-    return [self randomIntegerBetween:startingAt and:INT32_MAX];
+    return [self randomIntegerBetween:startingAt and:kINTMAX];
 }
 
 + (NSNumber *)randomIntegerBetween:(int32_t)min and:(int32_t)max
@@ -61,10 +64,124 @@
 + (NSNumber *)randomFloatBetween:(float)min and:(float)max
 {
     float diff = max - min;
-    unsigned int rInt = arc4random_uniform(INT32_MAX) + 1;
-    float r = ((float) rInt / INT32_MAX * diff) + min;
+    unsigned int rInt = arc4random_uniform(kINTMAX) + 1;
+    float r = ((float) rInt / kINTMAX * diff) + min;
     
     return @(r);
 }
 
+
+#pragma mark - Helpers
+
++ (BOOL)returnManyValue
+{
+    return ![self returnFewValue];
+}
+
+// Asymmetry tending to 85% NOs vs 15% YESs
++ (BOOL)returnFewValue
+{
+    u_int32_t r10 = arc4random_uniform(10) + 1; // In [1, 10]
+    if ((r10 % 3) == 0) {
+        return YES; // Only 15% of the times (propability)
+    }
+    return NO;
+}
+
 @end
+
+
+@implementation GZNumbers (Booleans)
+
++ (NSNumber *)yesOrNo
+{
+    u_int32_t rndBool = arc4random_uniform(2);
+    return rndBool == 1 ? @YES : @NO;
+}
+
++ (NSNumber *)yesOrNoMostly:(BOOL)m
+{
+    NSNumber *randomBool = [self yesOrNo];
+    if (m == randomBool.boolValue) {
+        return randomBool;
+    }
+    return [self returnManyValue] ? @(m) : randomBool;
+}
+
+@end
+
+
+@implementation GZNumbers (NaturalNumbers)
+
++ (NSNumber *)integerN
+{
+    return @(arc4random_uniform(kINTMAX));
+}
+
++ (NSNumber *)integerNNonZero
+{
+    return @(arc4random_uniform(kINTMAX) + 1);
+}
+
++ (NSNumber *)integerNLessOrEqual:(u_int32_t)max
+{
+    return [self integerNBetween:0 and:max];
+}
+
++ (NSNumber *)integerNBiggerOrEqual:(u_int32_t)min
+{
+    return [self integerNBetween:min and:kINTMAX];
+}
+
++ (NSNumber *)integerNBetween:(u_int32_t)min and:(u_int32_t)max
+{
+    NSInteger diff = (NSInteger)max - (NSInteger)min;
+    if (diff < 0) {
+        diff = -diff;
+        u_int32_t newMin = max;
+        u_int32_t newMax = min;
+        min = newMin;
+        max = newMax;
+    }
+    // Make sure number is never over INT32 limit in case it is used for sqlite, for eg.
+    if (min >= INT32_MAX) {
+        min = INT32_MAX;
+    }
+    if (max >= INT32_MAX) {
+        max = INT32_MAX;
+    }
+    if (diff == 0) {
+        return @(max);
+    }
+    u_int32_t rndInRange = min + arc4random_uniform((u_int32_t)diff + 1);
+    return @(rndInRange);
+}
+
+
+#pragma mark - Asymmetric
+
++ (NSNumber *)integerNBetween:(u_int32_t)min
+                          and:(u_int32_t)max
+                         many:(NSNumber *)m
+                          few:(NSNumber *)f
+{
+    NSNumber *random = [self integerNBetween:min and:max];
+    if (!m && !f) {
+        return random;
+    }
+    
+    if (m && m.integerValue != random.integerValue) {
+        if ([self returnManyValue]) {
+            return @(m.integerValue);
+        }
+    }
+    if (f && f.integerValue == random.integerValue) {
+        if (![self returnFewValue]) {
+            return [self integerNBetween:min and:max many:nil few:f];
+        }
+    }
+    return random;
+}
+
+@end
+
